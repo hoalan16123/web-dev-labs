40c03293c6a81b7b97f96a770c4c5f7f48b941e1
hoalan16123 <hoalan@pdx.edu>
2026-03-02 00:00:06 -0800

    Week 8

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -1,20 +1,27 @@
 // Exercise #1:
 // When the user clicks the 'copy' button, copy the user input to the output area
 
+
+$("document").ready(() => {
+  $("#copy").on("click", () => {
+    $("#output1").text($("#userInput1").val());
+
+  });
+});
 // Option #1
 // Fetch JavaScript objects representing specific elements in the DOM
-let userInput1 = document.querySelector('#userInput1');
-let copy = document.querySelector('#copy');
-let output1 = document.querySelector('#output1');
+// let userInput1 = document.querySelector('#userInput1');
+// let copy = document.querySelector('#copy');
+// let output1 = document.querySelector('#output1');
 
-// Add an event listener on the target element
-copy.addEventListener('click', handleClick);
+// // Add an event listener on the target element
+// copy.addEventListener('click', handleClick);
 
-// Callback function to handle event
-function handleClick(event) {
-  console.log('click event', event);
-  output1.textContent = userInput1.value;
-}
+// // Callback function to handle event
+// function handleClick(event) {
+//   console.log('click event', event);
+//   output1.textContent = userInput1.value;
+// }
 
 // Option #2
 // document.getElementById('copy').onclick = () => {
@@ -34,15 +41,22 @@ function handleClick(event) {
 // Exercise #2:
 // When the user enters input text, copy the user input to the output area
 
+
+$("document").ready(() => {
+  $("#userInput2").on("input", () => {
+    $("#output2").text($("#userInput2").val());
+
+  });
+});
 // Fetch JavaScript objects representing specific elements in the DOM
-let userInput2 = document.querySelector('#userInput2');
-let output2 = document.querySelector('#output2');
+// let userInput2 = document.querySelector('#userInput2');
+// let output2 = document.querySelector('#output2');
 
-// Add an event listener on the target element
-userInput2.addEventListener('input', handleInput);
+// // Add an event listener on the target element
+// userInput2.addEventListener('input', handleInput);
 
-// Callback function to handle event
-function handleInput(event) {
-  console.log('click event', event);
-  output2.textContent = userInput2.value;
-}
+// // Callback function to handle event
+// function handleInput(event) {
+//   console.log('click event', event);
+//   output2.textContent = userInput2.value;
+// }
